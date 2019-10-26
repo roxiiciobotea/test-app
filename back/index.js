9dadcca8519af0f50bec5fef5beca78cea25afcf
Alexandra-Roxana Ciobotea <alexandra-roxana.ciobotea@sap.com>
2019-10-26 20:41:44 +0300

    Adds db module. Connects from NodeJS to DB.

--- back/index.js
+++ back/index.js
@@ -1,5 +1,19 @@
-const app = require('express')();
 
-app.get('/', (req, res) => res.send('Start page'));
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("postgres://dbuser:dbpass@test-db:5432/testdb");
 
-app.listen(7000, () => console.log("Started"));
+return sequelize
+  .authenticate()
+  .then(() => {
+    const app = require('express')();
+
+    app.get('/', (req, res) => res.send('Startedddddddd'));
+
+    app.listen(7000, () => console.log("Startedddddddd"));
+
+    return app;
+  }).catch(err => {
+
+    console.log("ERROR " + err);
+
+  });
--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA IF NOT EXISTS test;
+CREATE SCHEMA IF NOT EXISTS testdb;
 
-CREATE TABLE IF NOT EXISTS test."users" (
+CREATE TABLE IF NOT EXISTS testdb."users" (
   id serial NOT NULL,
   username varchar(255),
   password varchar(255),
